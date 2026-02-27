e46e8963f0f2e1f19ac827e98883bed1474be686
Matheus Venâncio Scomparim <dscompm@dev.CORP.BTB>
2026-02-27 11:06:46 -0300

    inicio

---
+++ src/main/java/br/com/salgadosdalucia/api/ApiSalgadosdaluciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.salgadosdalucia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalgadosdaluciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSalgadosdaluciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/salgadosdalucia/api/ApiSalgadosdaluciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salgadosdalucia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSalgadosdaluciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
