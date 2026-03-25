ee98efa64b59fc2a73d9885e122805dd6b4ed502
matheus-vsm <scompinha@hotmail.com>
2026-03-24 23:10:05 -0300

    entidades cliente e endereco criadas

--- src/main/java/br/com/salgadosdalucia/api/model/Cliente.java
+++ src/main/java/br/com/salgadosdalucia/api/model/Cliente.java
@@ -1,10 +1,7 @@
 package br.com.salgadosdalucia.api.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "clientes")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Cliente {
 
     @Id
--- src/main/java/br/com/salgadosdalucia/api/model/Endereco.java
+++ src/main/java/br/com/salgadosdalucia/api/model/Endereco.java
@@ -1,24 +1,22 @@
 package br.com.salgadosdalucia.api.model;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Embeddable
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Endereco {
 
     private String logradouro;
-    private String bairro;
+    private String numero;
+    private String complemento;
     private String cep;
+    private String bairro;
     private String cidade;
     private String uf;
-    private String complemento;
-    private String numero;
 
 }
