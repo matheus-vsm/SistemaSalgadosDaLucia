5bc216616788df394a9c11de79e61b198f1b9c59
matheus-vsm <scompinha@hotmail.com>
2026-04-03 18:41:39 -0300

    inclusão de comentário no console do H2

---
+++ src/test/java/br/com/salgadosdalucia/api/service/ClienteServiceTest.java
@@ -0,0 +1,33 @@
+package br.com.salgadosdalucia.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteServiceTest {
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void listarTodos() {
+    }
+
+    @Test
+    void buscarPorId() {
+    }
+
+    @Test
+    void buscarPorNome() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void desativar() {
+    }
+
+}
