206b91c37fe402c3b5064d22507bcc3cb29cb41a
matheus-vsm <scompinha@hotmail.com>
2026-04-13 00:34:03 -0300

    cascade type no itens em pedido

--- src/main/java/br/com/salgadosdalucia/api/pedido/Pedido.java
+++ src/main/java/br/com/salgadosdalucia/api/pedido/Pedido.java
@@ -23,22 +23,32 @@ public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
-    @OneToMany(mappedBy = "pedido")
+
+    @OneToMany(mappedBy = "pedido",
+            cascade = CascadeType.ALL, // propaga TODAS as operações (salvar, atualizar, deletar, etc.) do Pedido para ItemPedido automaticamente
+            orphanRemoval = true) // // remove do banco qualquer ItemPedido que for removido da lista "itens" do Pedido
     private List<ItemPedido> itens;
+
     @Embedded
     private Endereco enderecoEntrega;
+
     private BigDecimal valorTotal;
     private LocalDate dataPedido;
     private LocalDateTime dataEntrega;
+
     @Enumerated(EnumType.STRING)
     private StatusPedido status;
+
     @Enumerated(EnumType.STRING)
     private TipoEntrega tipoEntrega;
+
     @Enumerated(EnumType.STRING)
     private FormaPagamento formaPagamento;
+
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuarioResponsavel;
