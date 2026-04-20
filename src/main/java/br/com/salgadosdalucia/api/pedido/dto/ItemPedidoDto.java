ffb5682ed101a96adcd779aebdbc904163bf3673
matheus-vsm <scompinha@hotmail.com>
2026-04-20 13:52:14 -0300

    validacao

--- src/main/java/br/com/salgadosdalucia/api/pedido/dto/ItemPedidoDto.java
+++ src/main/java/br/com/salgadosdalucia/api/pedido/dto/ItemPedidoDto.java
@@ -2,11 +2,13 @@ package br.com.salgadosdalucia.api.pedido.dto;
 
 import br.com.salgadosdalucia.api.pedido.TipoPreco;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ItemPedidoDto(
         @NotNull(message = "ID do Salgado é obrigatório!")
         Long salgadoId,
         @NotNull(message = "Quantidade do Item é obrigatória!")
+        @Positive(message = "Quantidade do Item deve ser um número positivo!")
         Integer quantidade,
         @NotNull(message = "Tipo de Preço do Item é obrigatório!")
         TipoPreco tipoPreco
