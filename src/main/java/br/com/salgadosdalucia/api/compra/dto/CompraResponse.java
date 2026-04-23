09e225b25df8ad0e5de21ca4606028ab332040cc
Matheus Pessoal <scompinha@hotmail.com>
2026-04-23 15:37:33 -0300

    ajustes no compra

--- src/main/java/br/com/salgadosdalucia/api/compra/dto/CompraResponse.java
+++ src/main/java/br/com/salgadosdalucia/api/compra/dto/CompraResponse.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-public record CriacaoCompraResponse(
+public record CompraResponse(
         Long id,
         List<ItemCompraResponse> itens,
         BigDecimal valorTotal,
