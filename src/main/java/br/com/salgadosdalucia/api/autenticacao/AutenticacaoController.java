9da7335d05a37c4cda4eabe4b8c20c09a71cbc31
Matheus <scompinha@hotmail.com>
2026-08-11 10:42:44 -0300

    ajuste de rota: request mapping adicionado

--- src/main/java/br/com/salgadosdalucia/api/autenticacao/AutenticacaoController.java
+++ src/main/java/br/com/salgadosdalucia/api/autenticacao/AutenticacaoController.java
@@ -13,9 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/autenticacao")
 @Validated
 @RequiredArgsConstructor
 @Tag(name = "Autenticação", description = "Endpoints públicos para autenticação e renovação de tokens")
